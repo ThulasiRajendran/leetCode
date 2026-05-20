827399197dace648fafcec000a403d2ee0fa2063
ThulasiRajendran <83634907+ThulasiRajendran@users.noreply.github.com>
2026-05-20 23:29:06 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
+ class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+
+        return new int[]{}; // No solution found
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = solution.twoSum(nums, target);
+
+        if (result.length == 2) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+            System.out.println("Numbers: " + nums[result[0]] + ", " + nums[result[1]]);
+        } else {
+            System.out.println("No solution found.");
+        }
+    }
+}
